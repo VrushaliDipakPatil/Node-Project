f38549998bb6bfd87a326b2ad5653b0ed4d4a39e
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-09-24 16:56:03 +0530

    clean up

--- app.js
+++ app.js
@@ -1,56 +1,8 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    fs.readFile("formValues.txt", (err, data) => {
-      res.end(
-        `<h1>${data.toString()}</h1>
-            <form action="/message" method="POST">
-            <label>Name:</label>
-            <input type="text" name="username"></input>
-            <button type="submit">Add</button>
-            </form>
-            `,
-      );
-    });
-  } else {
-    if (req.url === "/message") {
-      res.setHeader("Content-Type", "text/html");
-      let body = [];
-      req.on("data", (chunks) => {
-        console.log(chunks);
-        body.push(chunks);
-      });
-
-      req.on("end", () => {
-        let buffer = Buffer.concat(body);
-        console.log(buffer);
-
-        let formData = buffer.toString();
-        console.log(formData);
-
-        const formValues = formData.split("=")[1];
-        fs.writeFile("formValues.txt", formValues, (err) => {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          res.end();
-        });
-      });
-    } else {
-      if (req.url === "/read") {
-        fs.readFile("formValues.txt", (err, data) => {
-          console.log(data.toString());
-          res.end(`<h1>${data.toString()}</h1>`);
-        });
-      }
-    }
-  }
-});
+routes.testFunction();
+const server = http.createServer(routes.handler);
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
