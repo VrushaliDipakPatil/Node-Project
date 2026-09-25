4c7a44e4f52888ef94a0022dc719217984a69505
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-09-25 15:37:15 +0530

    middlewear

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Middleware 1 executed");
-  next();
-});
+const addUser=(req,res,next)=>{
+    req.user="Guest";
+    next();
+}
 
-app.use((req, res, next) => {
-  console.log("Middleware 2 executed");
-  res.send("<h1>Server is up and running on port 3000! Ready to handle requests</h1>");
+app.get("/welcome",addUser,(req,res)=>{
+    res.send(`<h1>Welcome ${req.user}!</h1>`);
 });
 
 app.listen(3000,()=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests");
+    console.log("Server is running");
 });
