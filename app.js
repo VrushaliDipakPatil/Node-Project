105c1f851f49063cca43b5db50a94a3a77044b34
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-09-25 15:54:55 +0530

    routes

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const addUser=(req,res,next)=>{
-    req.user="Guest";
-    next();
-}
+app.get("/orders",(req,res)=>{
+    res.send("Here is the list of all orders.");
+});
 
-app.get("/welcome",addUser,(req,res)=>{
-    res.send(`<h1>Welcome ${req.user}!</h1>`);
+app.post("/orders",(req,res)=>{
+    res.send("A new order has been created.");
+});
+
+app.get("/users",(req,res)=>{
+    res.send("Here is the list of all users.");
+});
+
+app.post("/users",(req,res)=>{
+    res.send("A new user has been added.");
 });
 
 app.listen(3000,()=>{
-    console.log("Server is running");
+    console.log("Server is running on http://localhost:3000");
 });
