94ed0d5790bc472eefb1fd03d7c04701e9e6ff9c
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-09-26 13:10:38 +0530

    route and query params

--- app.js
+++ app.js
@@ -2,26 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/products ",(req,res)=>{
-    res.send("Here is the list of all products.");
+app.get("/", (req, res) => {
+    res.send("Server is working!");
 });
 
-app.post("/products ",(req,res)=>{
-    res.send("A new product has been added.");
+app.get("/welcome/:username",(req,res)=>{
+    const username=req.params.username;
+    const role=req.query.role;
+    res.send(`Hello ${username}! You are a ${role}.`);
 });
 
-app.get("/categories ",(req,res)=>{
-    res.send("Here is the list of all categories.");
-});
-
-app.post("/categories ",(req,res)=>{
-    res.send("A new category has been created.");
-});
 
 app.use((req,res)=>{
     res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
-app.listen(4000,()=>{
-    console.log("Server is running on http://localhost:4000");
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000");
 });
